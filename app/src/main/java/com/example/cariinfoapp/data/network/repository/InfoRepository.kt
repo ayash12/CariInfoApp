699ff7ef7a6d2fe3aa1a22350df3ea481306adc5
ayash12 <ayashabdus@gmail.com>
2025-10-31 19:17:39 +0700

    feat(repo): integrate Room fallback into InfoRepository

--- app/src/main/java/com/example/cariinfoapp/data/network/repository/InfoRepository.kt
+++ app/src/main/java/com/example/cariinfoapp/data/network/repository/InfoRepository.kt
@@ -1,25 +1,32 @@
 package com.example.cariinfoapp.data.network.repository
 
 import com.example.cariinfoapp.data.database.model.Article
+import com.example.cariinfoapp.data.local.InfoDAO
+import com.example.cariinfoapp.data.local.toEntity
+import com.example.cariinfoapp.data.local.toModel
 import com.example.cariinfoapp.data.network.client.RetrofitClient
 import com.example.cariinfoapp.data.network.service.InfoApiService
 import javax.inject.Inject
 
 class InfoRepository @Inject constructor(
-    private val api: InfoApiService
+    private val api: InfoApiService,
+    private val dao: InfoDAO
 ) {
-//    private val api = RetrofitClient.instance.create(InfoApiService::class.java)
 
-    suspend fun getTopHeadlines(): List<Article> {
+
+    suspend fun getTopHeadlines(): Pair<List<Article>, Boolean> {
         return try {
             val response = api.getTopHeadlines()
             if (response.isSuccessful) {
-                response.body()?.articles.orEmpty()
+                val articles = response.body()?.articles.orEmpty()
+                dao.clearNews()
+                dao.insertAll(articles.map { it.toEntity() })
+                Pair(articles, false) // false = data dari network
             } else {
-                emptyList()
+                Pair(dao.getAllNews().map { it.toModel() }, true) // true = data cache
             }
         } catch (e: Exception) {
-            emptyList()
+            Pair(dao.getAllNews().map { it.toModel() }, true)
         }
     }
 }
